5fd1f7d6f51706a895f4497c807898a0657c9dac
hlebon <hanslgarcia@gmail.com>
2017-11-16 18:21:56 -0500

    practice: practice some fundamentals principles
    practice props, filter and map to render UI

--- app/index.js
+++ app/index.js
@@ -1,17 +1,48 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+const personas = [
+    {nombre: "Hans", id: 1,    esAmigo:true},
+    {nombre: "Claudia", id: 2, esAmigo:false},
+    {nombre: "Itzel", id: 3,   esAmigo:true},
+    {nombre: "Daniel", id: 4,  esAmigo:false}
+]
+
+
 class App extends Component{
     render(){
         return (
             <div>
-                <h1>Hola {this.props.name}</h1>
+                <div>
+                    <h2>Total</h2>
+                    {
+                        this.props.personas.map( persona =>(
+                            <div key={persona.id}>{persona.nombre}</div>
+                        ))
+                    }
+                </div>
+                <div>
+                    <h2>Amigos</h2>
+                    {
+                        this.props.personas.filter( persona => persona.esAmigo )
+                        .map( amigo =>  <div key={amigo.id}>{amigo.nombre}</div> )
+                    }
+                </div>
+                <div>
+                    <h2>No amigos</h2>
+                    {
+                        this.props.personas.filter( persona => !persona.esAmigo )
+                        .map( amigo =>  <div key={amigo.id}>{amigo.nombre}</div> )
+                    }
+                </div>
             </div>
         )
     }
 }
 
 ReactDOM.render(
-    <App name={"Hans"}/>,
+    <App personas={personas}/>,
     document.getElementById('app')
 );
+
+
