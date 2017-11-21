307c7317c14f85c308215548a20f2e543aa0324b
hlebon <hanslgarcia@gmail.com>
2017-11-21 17:07:59 -0500

    feat: a list of popular repos

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react'
 import { PropTypes } from 'prop-types'
-
+import * as api from '../utils/api'
 
 function SelectLanguage(props){
     let langs = [ "All", "Java", "C#", "PHP", "Javascript", "Python" ]
@@ -10,8 +10,8 @@ function SelectLanguage(props){
                 return (
                     <li 
                         key={lang} 
-                        onClick={() => this.props.onSelectLang(lang)}
-                        style={lang === this.props.selectedLang ? { color: '#d0021b' } : {color: ''}}>
+                        onClick={() => props.onSelectLang(lang)}
+                        style={lang === props.selectedLang ? { color: '#d0021b' } : {color: ''}}>
                         {lang}
                     </li>
                 )
@@ -20,8 +20,36 @@ function SelectLanguage(props){
     )
 }
 
+function RepoGrid(props){
+    return(
+        <ul>
+            {props.repos.map((item, index)=>{
+                return (
+                    <li key={item.name}>
+                        <div>#{index+1}</div>
+                        <ul>
+                            <li>
+                                <img src={item.owner.avatar_url} alt={`Avatar for ${item.owner.login}`}></img>
+                            </li>
+                            <li>
+                                <a href={item.html_url}>{item.name}</a>
+                            </li>
+                            <li>@{item.owner.login}</li>
+                            <li>{item.stargazers_count}</li>
+                        </ul>
+                    </li>
+                )
+            })}
+        </ul>
+    )
+}
 
-SelectLanguage.PropTypes = {
+RepoGrid.propTypes = {
+    repos: PropTypes.array.isRequired
+}
+
+
+SelectLanguage.propTypes = {
     selectedLang: PropTypes.string.isRequired,
     onSelectLang: PropTypes.func.isRequired
 }
@@ -29,22 +57,39 @@ SelectLanguage.PropTypes = {
 
 class Popular extends Component{
     state = {
-        selectedLang : "All"
+        selectedLang : "Java",
+        repos: []
+    }
+
+    componentDidMount(){
+        this.updateLang(this.state.selectedLang)
     }
 
     updateLang = (lang) => {
+
         this.setState({
-            selectedLang: lang
+            repos: []
+        })
+
+        api.fetchPopularRepos(lang)
+        .then((repos)=>{
+            console.log(repos)
+           this.setState({
+               selectedLang: lang,
+               repos: repos
+           }) 
         })
     }
 
     render(){
+        console.log(this.state.repos.length)
         return (
             <div>
                 <SelectLanguage
                     selectedLang={this.state.selectedLang}
                     onSelectLang={this.updateLang}
                 />
+                {this.state.repos.length > 0 ? <RepoGrid repos={this.state.repos}/> : <p>LOADING...</p> }
             </div>
         )
     }
